4939bd9f00db46060ce3216448a1ab8f7c740718
Tripledobleu <Tripledobleu@tripledobleu.local>
2016-09-08 03:39:38 -0500

    Correcto

---
+++ juegoDeMemoria.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground que evalúa un rango de números en base a 4 reglas de impresión.
+
+import UIKit
+
+var lista = 1...100
+
+
+for numero:Int in lista{
+    if numero % 5 == 0{
+        print("\(numero) ¡Bingo!") /* Si el número es divisible entre 5, imprime: # el número  + “¡Bingo!”*/
+    }else
+        if numero % 2 == 0{
+            print("\(numero) Par") /* Si el número es par, imprime: # el número + “Par”*/
+        }else{
+            print("\(numero) impar") /* Si el número es impar, imprime: # el número + “Impar”*/
+    }
+    if numero > 30 && numero < 40{
+        print("Viva Swift :D") /* Si el número se encuentra dentro de un rango del 30 al 40, imprime: # el número +  “Viva Swift!!!” */
+        
+    }
+}
